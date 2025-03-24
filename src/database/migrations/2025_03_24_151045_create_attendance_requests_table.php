59bf0b95dad5383791c4b338caff7997f3b8ce4d
yuriko <futabagusa.yuriko@gmail.com>
2025-03-24 15:37:57 +0900

    マイグレーションファイルの作成と設定

---
+++ src/database/migrations/2025_03_24_151045_create_attendance_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendanceRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attendance_id')->constrained()->cascadeOnDelete();
+            $table->text('request_reason');
+            $table->foreignId('status_id')->constrained('attendance_request_statuses')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance_requests');
+    }
+}
